5c6dd7d694a5699eeffad49d5c9ffa6895ef7c7a
Geoff Cohn <geoffcohn@gmail.com>
2015-04-19 08:13:41 -0500

    edit same type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     respond_to do |format|
-      format.json { render json: @user root: false}
+      format.json { render json: @user, root: false}
     end
   end
 
