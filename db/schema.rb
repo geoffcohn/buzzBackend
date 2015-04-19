e2d4622a412085fa9e041ad571d07b598d0b1c73
Geoff Cohn <geoffcohn@gmail.com>
2015-04-18 23:24:23 -0500

    db:create

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150418232135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.integer  "phone_number"
