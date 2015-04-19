ee9a1e953bb584c722b2000bedd5c7e85815ab1b
Geoff Cohn <geoffcohn@gmail.com>
2015-04-19 04:08:47 -0500

    relationships work

---
+++ app/controllers/relationship_controller.rb
@@ -0,0 +1,26 @@
+class RelationshipController < ApplicationController
+  before_action :set_relationship, only [:update, :destroy]
+  def create
+    @relationship = Relationship.new(relationship_params)
+    @relationship.save
+  end
+
+  def update
+    @relationship.update(relationship_params)
+  end
+
+  def 
+    @relationship.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_relationship
+      @relationship = Relationship.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def relationship_params
+      params.permit(:follower_id, :friend_id)
+    end
+end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,5 +3,5 @@ class Relationship < ActiveRecord::Base
   belongs_to :friend, class_name: "User"
 
   validates :follower_id, presence: true
-  validates :followed_id, presence: true
+  validates :friend_id, presence: true
 end
