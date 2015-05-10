e038d159467405833a0cdae480e4648f891c2445
Geoff Cohn <geoffcohn@gmail.com>
2015-05-10 01:07:31 -0500

    deleted all remove username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,6 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    user_params = user_params.delete(username)
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
